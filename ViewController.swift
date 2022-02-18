124c54b01b3c50e34b330c9d6bd67c1cbca5ffee
Carter Brehm <carter@brehmit.com>
2022-02-17 18:43:11 -0600

    Initialize Xcode project in repository, and move all files to top-level.

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  malone-swift-app
+//
+//  Created by Carter Brehm on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
