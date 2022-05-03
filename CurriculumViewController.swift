7d1025927181156aebb3095ec7465a71054f8a68
Carter Brehm <carter@brehmit.com>
2022-05-02 22:51:00 -0500

    Add basic functionality to curriculum page: - Listing PDFs - Styling (same as announcements) - no opening yet

--- CurriculumViewController.swift
+++ CurriculumViewController.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import WebKit
+import PDFKit
+
 
 class CurriculumViewController: UITableViewController {
     
-
+    
+    
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -26,4 +33,32 @@ class CurriculumViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let path = Bundle.main.resourcePath!
+        
+        let enumerator = FileManager.default.enumerator(atPath: path)
+        let filePaths = enumerator?.allObjects as! [String]
+        let txtFilePaths = filePaths.filter{$0.contains(".pdf")}
+        return txtFilePaths.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let path = Bundle.main.resourcePath!
+        
+        let enumerator = FileManager.default.enumerator(atPath: path)
+        let filePaths = enumerator?.allObjects as! [String]
+        let txtFilePaths = filePaths.filter{$0.contains(".pdf")}
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PDFCell", for: indexPath)
+
+        cell.textLabel?.text = txtFilePaths[indexPath.row]
+        cell.detailTextLabel?.text = "poop cock"
+        
+
+            return cell
+    }
+    
+    
 }
